9786065868952bcaf747d5dcd94c517f1de66560
Wonseok Chae <wochae@microsoft.com>
2015-02-12 14:22:41 -0800

    Add a second file

---
+++ Second.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ConsoleApplication48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Wolrd");
+            Console.WriteLine("Hello, See");
+            Console.WriteLine("Hello, Moon");
+        }
+    }
+}
