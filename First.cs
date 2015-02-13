5fdda192ad3ff3d8c96ea24447216eaf41536192
Wonseok Chae <wochae@microsoft.com>
2015-02-13 10:18:39 -0800

    update output

--- First.cs
+++ First.cs
@@ -9,6 +9,7 @@ namespace ConsoleApplication48
             Console.WriteLine("Hello, Wolrd");
             Console.WriteLine("Hello, See");
             Console.WriteLine("Hello, Moon");
+            Console.WriteLine("Hello, Moon 2");
         }
     }
 }
